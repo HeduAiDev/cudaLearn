da1af44bd2a7e43046d744ad49caa406978ea951
hedu <15156970070@163.com>
2024-05-22 23:25:40 +0800

    compile settings change

--- 3rd/update.bat
+++ 3rd/update.bat
@@ -1,14 +1,25 @@
 @echo off
-rmdir /s /q cutlass.origin
+setlocal enabledelayedexpansion
 
+rem 删除cutlass.origin目录
+rd /s /q cutlass.origin
+
+rem 克隆cutlass仓库
 git clone https://github.com/NVIDIA/cutlass.git --depth=1 cutlass.origin
 
-rmdir /s /q cutlass
+rem 删除cutlass目录并创建新的cutlass目录
+rd /s /q cutlass
 mkdir cutlass
-xcopy /E /I cutlass.origin\include cutlass
 
-cd cutlass.origin
-git rev-parse HEAD > ../cutlass/readme
-cd ..
+rem 复制cutlass.origin/include到cutlass
+robocopy cutlass.origin\include cutlass\include /mir
 
-rmdir /s /q cutlass.origin
+rem 获取cutlass.origin的HEAD并写入cutlass/readme
+for /f "delims=" %%i in ('git -C cutlass.origin rev-parse HEAD') do (
+    echo %%i > cutlass\readme
+)
+
+rem 删除cutlass.origin目录
+rd /s /q cutlass.origin
+
+endlocal
